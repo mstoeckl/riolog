285e1a054373be4fbbdc9a5eebee9e9405b69f05
robotics <robotics@home>
2014-10-11 19:57:21 -0400

    update: still not good

--- src/netconsole2/views/SampleView.java
+++ src/netconsole2/views/SampleView.java
@@ -1,7 +1,11 @@
 package netconsole2.views;
 
-
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.SocketException;
 import java.nio.channels.DatagramChannel;
 
@@ -19,22 +23,18 @@ import org.eclipse.ui.*;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.SWT;
 
-
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
@@ -51,38 +51,44 @@ public class SampleView extends ViewPart {
 	private Action doubleClickAction;
 
 	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
+	 * The content provider class is responsible for providing objects to the
+	 * view. It can wrap existing objects in adapters or simply return objects
+	 * as-is. These objects may be sensitive to the current input of the view,
+	 * or ignore it and always show the same content (like Task List, for
+	 * example).
 	 */
-	 
+
 	class ViewContentProvider implements IStructuredContentProvider {
 		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
 		}
+
 		public void dispose() {
 		}
+
 		public Object[] getElements(Object parent) {
 			return new String[] { "One", "Two", "Three" };
 		}
 	}
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
+
+	class ViewLabelProvider extends LabelProvider implements
+			ITableLabelProvider {
 		public String getColumnText(Object obj, int index) {
 			return getText(obj);
 		}
+
 		public Image getColumnImage(Object obj, int index) {
 			return getImage(obj);
 		}
+
 		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().
-					getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+			return PlatformUI.getWorkbench().getSharedImages()
+					.getImage(ISharedImages.IMG_OBJ_ELEMENT);
 		}
 	}
+
 	class NameSorter extends ViewerSorter {
 	}
+
 
 	/**
 	 * The constructor.
@@ -90,39 +96,132 @@ public class SampleView extends ViewPart {
 	public SampleView() {
 	}
 
-	Text text;
+	public static String getPacket(DatagramSocket socket, byte[] buf) {
+		DatagramPacket p = new DatagramPacket(buf, buf.length);
+		try {
+			socket.receive(p);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return new String(p.getData());
+	}
 	
+	public static DatagramSocket makeRecvSocket() {
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket(null);
+			socket.setReuseAddress(true);
+			socket.bind(new InetSocketAddress(InetAddress.getLoopbackAddress(), 6666));
+		} catch (SocketException e) {
+			e.printStackTrace();
+			socket.close();
+			return null;
+		}
+		return socket;
+	}
+	
+	public static class UDPReceiver implements Runnable {
+		SampleView view;
+
+		public UDPReceiver(SampleView v) {
+			view = v;
+
+		}
+
+		public void start() {
+			Thread t = new Thread(this);
+			t.setDaemon(true);
+			t.start();
+		}
+
+		@Override
+		public void run() {
+			DatagramSocket socket = makeRecvSocket();
+			if (socket == null) 
+				return;
+			byte[] buf = new byte[4096];
+			while (true) {			
+				System.err.println("PREPACK");
+				String s = getPacket(socket, buf);
+				if (s != null) {
+					view.appendText(new String(buf));
+				}
+			}
+			//socket.close();
+		}
+	}
+
+	Text text;
+
 	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
+	 * This is a callback that will allow us to create the viewer and initialize
+	 * it.
 	 */
 	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
+				| SWT.V_SCROLL);
 		viewer.setContentProvider(new ViewContentProvider());
 		viewer.setLabelProvider(new ViewLabelProvider());
 		viewer.setSorter(new NameSorter());
 		viewer.setInput(getViewSite());
-		
-		text = new Text(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.READ_ONLY);
+
+		text = new Text(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL
+				| SWT.READ_ONLY);
 		text.append("TEST");
-		
-		// TODO: tie this in
-		try {
-			DatagramSocket socket = new DatagramSocket(6666);
-			socket.close();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		
+
+		new Thread(() -> (appendText("WOINK"))).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				appendText("NOINK");
+			}
+		}).start();
+
 		// Create the help context id for the viewer's control
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "netconsole2.viewer");
+		PlatformUI.getWorkbench().getHelpSystem()
+				.setHelp(viewer.getControl(), "netconsole2.viewer");
 		makeActions();
 		hookContextMenu();
 		hookDoubleClickAction();
 		contributeToActionBars();
+		//new UDPReceiver(this).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					appendText(String.valueOf(System.currentTimeMillis())
+							+ "\n");
+				}
+
+			}
+		}).start();
+		
+		String s = getPacket(makeRecvSocket(), new byte[4096]);
+		System.err.print(s);
+		appendText(s);
+		
+	}
+
+	public void appendText(final String string) {
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				if (text.isDisposed())
+					return;
+				text.append(string);
+			}
+		});
+
 	}
 
 	private void hookContextMenu() {
@@ -156,7 +255,7 @@ public class SampleView extends ViewPart {
 		// Other plug-ins can contribute there actions here
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 	}
-	
+
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(action1);
 		manager.add(action2);
@@ -170,9 +269,9 @@ public class SampleView extends ViewPart {
 		};
 		action1.setText("Action 1");
 		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+
 		action2 = new Action() {
 			public void run() {
 				showMessage("Action 2 executed?");
@@ -180,13 +279,14 @@ public class SampleView extends ViewPart {
 		};
 		action2.setText("Action 2");
 		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
 		doubleClickAction = new Action() {
 			public void run() {
 				ISelection selection = viewer.getSelection();
-				Object obj = ((IStructuredSelection)selection).getFirstElement();
-				showMessage("Double-click detected on "+obj.toString());
+				Object obj = ((IStructuredSelection) selection)
+						.getFirstElement();
+				showMessage("Double-click detected on " + obj.toString());
 			}
 		};
 	}
@@ -198,11 +298,10 @@ public class SampleView extends ViewPart {
 			}
 		});
 	}
+
 	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			viewer.getControl().getShell(),
-			"Sample View",
-			message);
+		MessageDialog.openInformation(viewer.getControl().getShell(),
+				"Sample View", message);
 	}
 
 	/**
